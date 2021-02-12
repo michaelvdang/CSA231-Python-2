ead99211c1ee77aee2647479bb0527e0ca02db30
ksim2-occ <54510732+ksim2-occ@users.noreply.github.com>
2021-02-11 16:57:18 -0800

    Add files via upload
    Pep8 format check - ksim2-occ, 2/11/21

--- pig.py
+++ pig.py
@@ -1,54 +1,57 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Feb  8 12:35:13 2021
-
 @author: Michael
-Game of Pigs: like a dice rolling game with points and chance to lose all 
-points and turn or lose the game altogether. 
+Game of Pigs: like a dice rolling game with points and chance to lose all
+points and turn or lose the game altogether.
     - game has 2 players, each take turn to pass or roll
     - game ends when a player reached target score or one is eliminated
-v2. - Let players choose number of players playing at the same time 
+v2. - Let players choose number of players playing at the same time
 """
 import random
 
 # -1 scores means player eliminated
+
+
 class GameOfPigs(object):
     def __init__(self, num_players: int = 2):
         self.players = []
         self.num_players = num_players
-        self.scores = []    
+        self.scores = []
         self.current_turn = 0
         # could be list of dictionary for name of players as key
         self.winning_score = None
-        self.scoring_table = {0 : 0,
-                              1 : 5,
-                              2 : 5,
-                              3 : 10,
-                              4 : 15}
+        self.scoring_table = {0: 0,
+                              1: 5,
+                              2: 5,
+                              3: 10,
+                              4: 15}
         self.scoring_names = {
-            0 : 'Sider', 
-            1 : 'Razorback',
-            2 : 'Trotter',
-            3 : 'Snouter',
-            4 : 'Leaning Jowler'
+            0: 'Sider',
+            1: 'Razorback',
+            2: 'Trotter',
+            3: 'Snouter',
+            4: 'Leaning Jowler'
         }
-        
+
     def roll(self) -> (int, int, int):
-        return (random.randint(0, 4), random.randint(0, 4), random.randint(0, 99))
-        # outcome - (pig 1 position, pig 2 position, 
+        return (random.randint(0, 4), random.randint(0, 4),
+                random.randint(0, 99))
+        # outcome - (pig 1 position, pig 2 position,
         #               piggyback (1%) or oinker(5%))
-        
+
     def roll_result(self, outcome):
         print(f"You rolled {self.scoring_names[outcome[0]]}",
               f"and {self.scoring_names[outcome[1]]}")
 
     def score(self, outcome: (int, int), current_score: int) -> int:
-        if outcome[2] < 1: # piggyback 1%
+
+        if outcome[2] < 1:  # piggyback 1%
             self.scores[self.current_turn] = -1
             self.num_players -= 1
             self.current_turn = (self.current_turn + 1) % len(self.players)
             print("Uh oh, Piggyback! You've been eliminated :(")
-        elif outcome[2] < 21: # oinker 20%
+        elif outcome[2] < 21:  # oinker 20%
             self.scores[self.current_turn] = 0
             self.current_turn = (self.current_turn + 1) % len(self.players)
             print("Oinker! You've lost all your points and your turn!")
@@ -74,27 +77,27 @@ class GameOfPigs(object):
                 score += self.scoring_table[outcome[1]]
                 self.scores[self.current_turn] += score
                 print(f"Mixed bag, You scored {score} points")
-        
+
     def play_game(self):
         print("Game starts")
-        num_players = int(input("How many are playing?"))
+        num_players = int(input("How many are playing? "))
         while num_players < 2:
             print("Must have more than 1 player to play this game")
-            num_players = int(input("How many are playing?"))
+            num_players = int(input("How many are playing? "))
         for i in range(num_players):
             name = input(f'Enter player {i + 1} name: ')
             self.players.append(name)
             self.scores.append(0)
         self.winning_score = int(input("Enter the target score: "))
-        
+
         # self.winning_score = 100
         # self.players.append('Mike')
         # self.scores.append(0)
         # self.players.append('Joe')
         # self.scores.append(0)
-            
-        while (self.scores[self.current_turn] < self.winning_score 
-               and self.num_players > 1):
+
+        while (self.scores[self.current_turn] <
+               self.winning_score and self.num_players > 1):
             print('Current turn', self.players[self.current_turn])
             btn = input("Do you want to roll(r) or pass(p)? ").lower()
             # btn = 'r'
@@ -114,11 +117,9 @@ class GameOfPigs(object):
 
             for i in range(len(self.players)):
                 print(self.players[i], 'has', self.scores[i], 'points')
-            
+
             # input(' next round ')
         print('Game over and the winner is', self.players[self.current_turn],
               'with', self.scores[self.current_turn], 'points')
-                
 game = GameOfPigs()
 game.play_game()
-
